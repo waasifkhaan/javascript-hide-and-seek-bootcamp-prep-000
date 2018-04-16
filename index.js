38835ef279b016447ad48ed8a199822e92cd51ca
Wasif Khan <waasif.khaan@gmail.com>
2018-04-16 19:19:48 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+function nestedTarget(nested,target){
+
+  const n = document.getElementById('nested')
+  const c = n.getElementsByClassName('target')
+  return c[0]
+}
+
+function increaseRankBy(n){
+  const rankedLists = document.querySelectorAll('.ranked-list')
+  for (let i =0 ; i<rankedLists.length; i++){
+    var x = rankedLists[i].children
+    for ( let z=0; z<x.length ; z++){
+    var q =parseInt(x[z].innerHTML)
+    x[z].innerHTML = q + n;
+  }
+  }
+}
